c2ec11962eb8ac79387b1df15904f9d8df77eed1
sairam-maurya <rams44258@gmail.com>
2023-12-28 12:38:35 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "tailwindcss/tailwind.css"; // Import Tailwind CSS
 
-function App() {
+const CountdownTimer = () => {
+  const [countdown, setCountdown] = useState(60);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const startCountdown = () => {
+    setIsRunning(true);
+  };
+
+  const stopCountdown = () => {
+    setIsRunning(false);
+  };
+
+  const resetCountdown = () => {
+    setCountdown(60);
+    setIsRunning(false);
+  };
+
+  const handleChange = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    setCountdown(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="max-w-md mx-auto p-4 border rounded shadow-md mt-8">
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Set Countdown Time (seconds):
+          <input
+            type="number"
+            value={countdown}
+            onChange={handleChange}
+            className="mt-1 p-2 border rounded-md w-full"
+          />
+        </label>
+      </div>
+      <div className="mb-4">
+        <p className="text-lg font-semibold">
+          Current Countdown: {countdown} seconds
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+      <div className="flex space-x-4">
+        <button
+          onClick={startCountdown}
+          disabled={isRunning}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
         >
-          Learn React
-        </a>
-      </header>
+          Start Countdown
+        </button>
+        <button
+          onClick={stopCountdown}
+          disabled={!isRunning}
+          className="bg-red-500 text-white px-4 py-2 rounded-md"
+        >
+          Stop Countdown
+        </button>
+        <button
+          onClick={resetCountdown}
+          className="bg-gray-300 text-gray-600 px-4 py-2 rounded-md"
+        >
+          Reset Countdown
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CountdownTimer;
